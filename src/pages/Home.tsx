12158f858865b0518c5eba5d6073bc12153caaed
Prakash Raut <prakash_raut@outlook.com>
2024-03-23 23:31:09 +0530

    Basic Tailwind Setup done

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import {Sidebar} from "../components/sidebar/Sidebar.tsx";
+import {Main} from "../components/main/Main.tsx";
+
+export function Home () {
+    return (
+        <>
+            <Sidebar/>
+            <Main/>
+        </>
+    )
+}
